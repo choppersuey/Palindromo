ba26e680130daec8ec576009405f052e07129a9b
choppersuey <gustavo.vsac@gmail.com>
2022-11-14 22:47:11 -0300

    First commit

---
+++ src/Desafio.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Desafio {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String palavra = sc.nextLine();
+        String palavraInvertida = new StringBuilder(palavra).reverse().toString();
+        String palavraDesvertida = new StringBuilder(palavraInvertida).reverse().toString();
+        if(palavra.equals(palavraInvertida)){
+            System.out.println("TRUE");
+        }else{
+            System.out.println("FALSE");
+        }
+    }
+}
